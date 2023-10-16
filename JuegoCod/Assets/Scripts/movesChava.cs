9f86094fc6bca0e07988c45b5635fb077e77091f
Bucky2 <bukygem84@gmail.com>
2023-10-16 11:01:04 -0600

    Avance de basic Gameplay Sprint02

--- JuegoCod/Assets/Scripts/movesChava.cs
+++ JuegoCod/Assets/Scripts/movesChava.cs
@@ -2,31 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movesChava : MonoBehaviour
+public class MovesChava : MonoBehaviour
 {
-    // Start is called before the first frame update
     public GameObject Player;
     public bool isMoving;
     public float horizontalMove;
     public float verticalMove;
 
-    // Update is called once per frame
+
     void Update()
     {
-        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
-        {
-            isMoving = true;
-            Player.GetComponent<Animator>().Play("walk");
-            horizontalMove = Input.GetAxis("Horizontal") * Time.deltaTime * 150; 
-            verticalMove = Input.GetAxis("Vertical") * Time.deltaTime * 4;
-            Player.transform.Rotate(0, horizontalMove, 0);
-            Player.transform.Translate(0, 0, verticalMove);
 
+           
+                {
+                    Player.GetComponent<Animator>().Play("walk");
+                }
+            }
         }
         else
         {
             isMoving = false;
             Player.GetComponent<Animator>().Play("Idle");
         }
+
+      
+        {
+            verticalMove = Input.GetAxis("Vertical") * Time.deltaTime * 3.8f;
+        }
+        else
+        {
+            verticalMove = Input.GetAxis("Vertical") * Time.deltaTime * 15.1f;
+        }
+        
+        horizontalMove = Input.GetAxis("Horizontal") * Time.deltaTime * 150;
+        Player.transform.Rotate(0, horizontalMove, 0);
+        Player.transform.Translate(0, 0, verticalMove);
     }
 }
